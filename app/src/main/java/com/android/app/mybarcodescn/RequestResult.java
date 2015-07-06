1c8612e9e80de367312514d520d393e669607779
huligun <huligun@gmail.com>
2015-07-06 22:26:02 +1000

    add method sendToStorer

---
+++ app/src/main/java/com/android/app/mybarcodescn/RequestResult.java
@@ -0,0 +1,11 @@
+package com.android.app.mybarcodescn;
+
+import java.io.Serializable;
+
+/**
+ * Created by CodeX on 06.07.2015.
+ */
+public class RequestResult implements Serializable {
+    int status;
+    String description;
+}
